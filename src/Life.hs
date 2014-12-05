755bcb0e3c3abe023c636adbcf8a59b9d845469f
Konstantin Saveljev <konstantin.saveljev@gmail.com>
2014-12-05 15:59:19 +0200

    Avoid method calls where possible. Another crazy boost

--- src/Life.hs
+++ src/Life.hs
@@ -28,23 +28,23 @@ randomBoard :: Width -> Height -> StdGen -> Board
 randomBoard w h = U.take (w * h) . U.unfoldr (Just . randomR (0, 1))
 
 nextGeneration :: Generation -> Generation
-nextGeneration !gen@(Generation w h cw gps brd) =
+nextGeneration gen@(Generation w h cw gps brd) =
     Generation w h cw gps (U.imap (nextCell gen) brd)
 
 {-# INLINE getCoords #-}
 getCoords :: Generation -> Int -> (Int, Int)
-getCoords !gen !idx = idx `quotRem` width gen
+getCoords (Generation w _ _ _ _) !idx = idx `quotRem` w
 
 {-# INLINE fromCoords #-}
 fromCoords :: Generation -> (Int, Int) -> Int
-fromCoords !gen (!x, !y) = x + (y * width gen)
+fromCoords (Generation w _ _ _ _) (!x, !y) = x + (y * w)
 
 countAlive :: Generation -> Int
 countAlive (Generation _ _ _ _ brd) = U.sum brd
 
 {-# INLINE nextCell #-}
 nextCell :: Generation -> Int -> Int -> Int
-nextCell !gen !idx !state
+nextCell gen@(Generation _ _ _ _ brd) !idx !state
   | nc < 2 || nc > 3 = 0
   | nc == 3 = 1
   | otherwise = state
@@ -61,8 +61,8 @@ nextCell !gen !idx !state
         + gn neg pos + gn zro pos + gn pos pos
     gn !mx !my
       | midx < 0 = 0
-      | midx >= U.length (board gen) = 0
-      | otherwise  = board gen U.! midx
+      | midx >= U.length brd = 0
+      | otherwise  = brd U.! midx
       where
         !midx = fromCoords gen (x + mx, y + my)
     !neg = -1
